6b5cb27a932e0e346319732090322b635cdc3925
MavHuber <mlh5401056@gmail.com>
2023-12-04 11:37:18 -0500

    Initialize variables

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-function startSession() {
-    window.location.href='session.html'
-}
+//Initialize variables
+var r1Count = 0;
+var r2Count = 0;
+var r3Count = 0;
+var fcrCount = 0;
+var trCount = 0;
+var demandCount = 0;
+var compCount = 0;
+
+var timer = 0;
+
