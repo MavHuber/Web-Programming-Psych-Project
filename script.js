8418b3d2f3ca0b5edd276fa8b0fe3a5e072d7af8
MavHuber <mlh5401056@gmail.com>
2023-12-06 13:43:02 -0500

    Utilize sessionStorage to bring over input data

--- script.js
+++ script.js
@@ -9,3 +9,63 @@ var compCount = 0;
 
 var timer = 0;
 
+function saveVar() {
+    var vidName = document.getElementById('vidName').value;
+    sessionStorage.setItem('vidName', vidName);
+
+    var sessNum = document.getElementById('session').value;
+    sessionStorage.setItem('sessNum', sessNum);
+
+    var vidLen = document.getElementById('length').value;
+    sessionStorage.setItem('vidLen', vidLen);
+    
+    var r1Text = document.getElementById('r1').value;
+    sessionStorage.setItem('r1Text', r1Text);
+
+    var r2Text = document.getElementById('r2').value;
+    sessionStorage.setItem('r2Text', r2Text);
+
+    var r3Text = document.getElementById('r3').value;
+    sessionStorage.setItem('r3Text', r3Text);
+
+    var fcrText = document.getElementById('fcr').value;
+    sessionStorage.setItem('fcrText', fcrText);
+
+    var trText = document.getElementById('tr').value;
+    sessionStorage.setItem('trText', trText);
+
+    var demandText = document.getElementById('demand').value;
+    sessionStorage.setItem('demandText', demandText);
+
+    var compText = document.getElementById('compliance').value;
+    sessionStorage.setItem('compText', compText);
+}
+
+function setButtons() {
+    var r1Text = sessionStorage.getItem('r1Text');
+    document.getElementById('r1Btn').innerHTML = r1Text;
+
+    var r1Text = sessionStorage.getItem('r1Text');
+    document.getElementById('r1Btn').innerHTML = r1Text;
+
+    var r1Text = sessionStorage.getItem('r1Text');
+    document.getElementById('r1Btn').innerHTML = r1Text;
+
+    var r1Text = sessionStorage.getItem('r1Text');
+    document.getElementById('r1Btn').innerHTML = r1Text;
+
+    var r1Text = sessionStorage.getItem('r1Text');
+    document.getElementById('r1Btn').innerHTML = r1Text;
+
+    var r1Text = sessionStorage.getItem('r1Text');
+    document.getElementById('r1Btn').innerHTML = r1Text;
+
+    var r1Text = sessionStorage.getItem('r1Text');
+    document.getElementById('r1Btn').innerHTML = r1Text;
+
+    var r1Text = sessionStorage.getItem('r1Text');
+    document.getElementById('r1Btn').innerHTML = r1Text;
+
+    var r1Text = sessionStorage.getItem('r1Text');
+    document.getElementById('r1Btn').innerHTML = r1Text;
+}
